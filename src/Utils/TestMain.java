1f7f280965de813492f9fd6592263a85339ca888
Zahar <yz.youssef.zahar@gmail.com>
2022-02-20 22:37:57 +0100

    Crud offre

--- src/Utils/TestMain.java
+++ src/Utils/TestMain.java
@@ -1,5 +1,13 @@
 package Utils;
 
+import Modules.Extern;
+import Modules.Interest;
+import Modules.Offre;
+import Services.InterestServices;
+import Services.OffreServices;
+import Utils.Enums.AdminDepartments;
+import Utils.Enums.OffreCategorie;
+import Utils.Enums.Roles;
 import Modules.Alert;
 import Modules.Annoucement;
 import Services.AlertServices;
@@ -88,5 +96,24 @@ postServices.deletePost(5);
         res.setContent("yes yes ! ");
         //respondedService.update(res);
         System.out.println(respondeds.toString());
+
+
+        OffreServices serviceOffre = OffreServices.getInstance();
+        InterestServices serviceIntrest = InterestServices.getInstance();
+
+        Interest interest = new Interest(1, 10020855);
+        Interest interest2 = new Interest(1, 10020855);
+        serviceIntrest.add(interest);
+        serviceIntrest.add(interest2);
+        Offre offre = new Offre("mohsen", "lll", OffreCategorie.Alternance, 10020855);
+        serviceOffre.add(offre);
+        List<Offre>offres=serviceOffre.getList();
+        System.out.println(offres);
+        Offre offre2=offres.get(0);
+        offre2.setCategory(OffreCategorie.Offre_De_Travail);
+        serviceOffre.update(offre2);
+        serviceOffre.delete(offre2);
+        System.out.println(serviceOffre.retrive(offre2.getIdOffer()));
+
     }
 }
