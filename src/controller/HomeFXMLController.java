269d443aad3b4d8e0ddd9c8f85b3dffc56cb554b
ala-hamadi <ala.hamadi.01@gmail.com>
2022-03-03 03:12:45 +0100

    Integrate post interface

---
+++ src/controller/HomeFXMLController.java
@@ -0,0 +1,93 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class HomeFXMLController{
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private Button SearchBtn;
+
+    @FXML
+    private Button PostBtn;
+
+    @FXML
+    private Button EventBtn;
+
+    @FXML
+    private Button ForumBtn;
+
+    @FXML
+    private Button OfferBtn;
+
+    @FXML
+    private Button AnnounceBtn;
+
+    @FXML
+    private Button AddBtn;
+
+    @FXML
+    private Button LogoutBtn;
+
+    @FXML
+    private StackPane content;
+
+    @FXML
+    void exitApp(ActionEvent event) {
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    void showPostMenu(ActionEvent event) {
+        try {
+            Parent menu = FXMLLoader.load(getClass().getResource("/view/UI/PostMenu.fxml"));
+            content.getChildren().removeAll();
+            content.getChildren().setAll(menu);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void showEventMenu(ActionEvent event) {
+}
+
+
+    @FXML
+    void showSearchList(MouseEvent event) {
+
+    }
+    @FXML
+    void AddAlert(ActionEvent event) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource("/view/UI/AddPost.fxml"));
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,50 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class main extends Application {
+
+    Parent parent;
+    Stage stage;
+
+    @Override
+  /* public void start(Stage primaryStage) {
+        this.stage = primaryStage;
+        try {
+            parent = FXMLLoader.load(getClass().getResource("/view/UI/EventFXML.fxml"));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("erreur");
+        }
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setTitle("Add and Show Persons");
+        stage.show();
+    }*/
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/UI/HomeTemplate.fxml"));
+        primaryStage.setTitle("Hello World");
+        try {
+            Image image = new Image("view/Images/appicon.png");
+            primaryStage.getIcons().add(image);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        primaryStage.setMaximized(true);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        //primaryStage.initStyle(StageStyle.DECORATED);
+        primaryStage.setScene(new Scene(root, 1080, 720));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
