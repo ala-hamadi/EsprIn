c56adc55b39ff85799504d36e88b33a233029927
eyakasmi <eyakasmy@gmail.com>
2022-03-04 22:56:54 +0100

    Add Event Interfaces

---
+++ src/Controllers/LoginController.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private Button LoginBtn;
+
+    @FXML
+    private Button RegisterBtn;
+
+    @FXML
+    private Button exitBtn;
+
+    @FXML
+    private TextField mail;
+
+    @FXML
+    private TextField password;
+}
